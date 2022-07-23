ae4650b2ee1a03362e718eb202f5457e7b055622
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-07-23 16:08:24 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+import { Container } from 'components/Container/Container';
+import { Section } from 'components/Section/Section';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import { Statistics } from 'components/Statistics/Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = key => {
+    // const buttonKey = event.target.textContent;
+    // console.log(key);
+
+    this.setState(prevState => {
+      return { [key]: prevState[key] + 1 };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return Object.keys(this.state).reduce(
+      (total, value) => total + this.state[value],
+      0
+    );
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        <GlobalStyle />
+        <Container>
+          <Section title="Please leave feddback">
+            <FeedbackOptions
+              options={this.state}
+              onLeaveFeedback={this.handleFeedback}
+            ></FeedbackOptions>
+          </Section>
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback}
+              positivePercentage={this.countPositiveFeedbackPercentage}
+            ></Statistics>
+          </Section>
+        </Container>
+      </>
+    );
+  }
+}
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  //   display: flex;
+`;
+export const Item = styled.li`
+  :not(:last-child) {
+    //     margin-bottom: 5px;
+  }
+`;
+
+export const Value = styled.span`
+  font-weight: 700;
+`;
